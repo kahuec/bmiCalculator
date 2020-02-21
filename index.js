8a2ae3a77da39690c496fc36fe81b186857292ab
Debasis Bhattacharya <debasisb@hawaii.edu>
2020-02-21 00:30:08 +0000

    Update BMI Calculator to only have route /bmiCalculator and also cut out index.html

--- index.js
+++ index.js
@@ -7,24 +7,8 @@ const app = express();
 // this allows the parsing of the html file using body parser
 app.use(bodyParser.urlencoded({extended: true}));
 
-//this sends the html file to the web page using the root directory
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html")
-});
-
-// this gets the response from the values in the web page
-app.post("/", function(req, res){
-
-  var num1 = Number(req.body.n1);
-  var num2 = Number(req.body.n2);
-
-// does the computation of the input variables, as numbers
-  var result = num1 * num2;
-
-// sends the results back to the web page as string
-  res.send("The result of the calculation is " + result);
-})
-//this gets the response from the web page to this placeholder
+//trimmed the code to only keep the route /bmiCalculator
+// to display - Open in New Tab, add /bmiCalculator to URL
 
 // this code is only invoked on the path /bmiCalculator
 app.get("/bmiCalculator", function(req, res){
