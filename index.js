3cdeb6e799fc4c9842e703981dc4d60c69b4aa74
kahuec <kahuec@hawaii.edu>
2020-02-26 03:10:31 +0000

    ladder code

--- index.js
+++ index.js
@@ -7,26 +7,23 @@ const app = express();
 // this allows the parsing of the html file using body parser
 app.use(bodyParser.urlencoded({extended: true}));
 
-//trimmed the code to only keep the route /bmiCalculator
-// to display - Open in New Tab, add /bmiCalculator to URL
-
-// this code is only invoked on the path /bmiCalculator
-app.get("/bmiCalculator", function(req, res){
-  res.sendFile(__dirname + "/bmiCalculator.html");
+// this code is only invoked on the path /ladderCalculator
+app.get("/ladderCalculator", function(req, res){
+  res.sendFile(__dirname + "/ladderCalculator.html");
 });
 
 // invoked on the submit button
-app.post("/bmiCalculator", function(req, res){
+app.post("/ladderCalculator", function(req, res){
 
 //converts the string input to a float number
-  var weight = parseInt(req.body.weight);
+  var depth = parseInt(req.body.depth);
   var height = parseInt(req.body.height);
 
 // does the computation of the input variables, as numbers
-  var bmi = weight / (height * weight);
+  var length = Math.sqrt(Math.pow (depth,2) + Math.pow (height,2));
 
 //display the result in 2 decimal places
-res.send("Your BMI is " + bmi.toFixed(2));
+res.send("Your ladder's length is " + length.toFixed(0));
 
 })
 
